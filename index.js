bd8e580433a152d58bc2835b03bda34530b130a8
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-03-08 15:55:59 -0300

    Added port as env variable.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const sqlite = require("sqlite");
 const dbConnection = sqlite.open("banco.sqlite", { Promise });
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
@@ -146,7 +148,7 @@ const init = async () => {
 
 init();
 
-app.listen(3000, err => {
+app.listen(port, err => {
   if (err) {
     console.log("Nao foi possivel iniciar o servidor");
   } else {
